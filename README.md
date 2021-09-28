9758ee14ce3426cf38d6024429ee6d43cb33c768
Andrey Trubitsyn <iverstoon@icloud.com>
2021-09-28 22:12:29 +0400

    refactoring tweak

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-
 npm run dev
+
 ### or
+
 yarn dev
-___________________________
+
+---
+
 Refactored code from test:
 
 ```JavaScript
@@ -10,10 +13,17 @@ function func1(s, a, b) {
   if (s.length == 0 ) {
         return -1;
     }
-  s1 = s.toLowerCase();
-  aIndex = s1.indexOf(a.toLowerCase());
-  bIndex = s1.indexOf(b.toLowerCase());
-
+  let aIndex = -1;
+  let bIndex = -1;
+  s1 = s.toLowerCase().split("");
+  s1.forEach((char, i) => {
+    if (char === a.toLowerCase()) {
+      aIndex = i;
+    }
+    if (char === b.toLowerCase()) {
+      bIndex = i;
+    }
+  })
   return Math.max(aIndex, bIndex);
 }
 ```
--- pages/providerPay/[id].tsx
+++ pages/providerPay/[id].tsx
@@ -104,37 +104,6 @@ const ProviderPay = ({provider, errors}) => {
     setEnteredAmount("");
   };
 
-  // const data = [
-  //   {
-  //     id: "1",
-  //     title: "MegaFon",
-  //     url: "../megafon.png",
-  //   },
-  //   {
-  //     id: "2",
-  //     title: "Beeline",
-  //     url: "../beeline.png",
-  //   },
-  //   {
-  //     id: "3",
-  //     title: "MTS",
-  //     url: "../MTS.png",
-  //   },
-  //   {
-  //     id: "4",
-  //     title: "TELE 2",
-  //     url: "../tele2.png",
-  //   },
-  //   {
-  //     id: "5",
-  //     title: "Tinkoff Mobile",
-  //     url: "../tinkoff.png",
-  //   },
-  // ];
-  // const router = useRouter();
-  //console.log(router);
-  // const { key } = router.query;
-  // const provider = data.find((item) => item.id == key);
   return (
     <>
       {/* {error && (
