11619f5c7b0d16b49a5b1cef516c8cf73352ff5f
Andrey Trubitsyn <iverstoon@icloud.com>
2021-09-28 19:45:08 +0400

    get styled components part 2

--- components/ProviderPay.js
+++ components/ProviderPay.js
@@ -1,9 +1,77 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import styles from "./ProviderPay.module.css";
+// import styles from "./ProviderPay.module.css";
+import styled from "styled-components";
 import Link from "next/link";
 import ErrorModal from "./ErrorModal";
 import MaskedInput from "react-text-mask";
+
+const PayWindow = styled.main`
+  width: 30rem;
+  height: auto;
+  padding: 0.5rem 1rem;
+  background-color: #afcfea;
+  border-radius: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const PayWindowImg = styled.img`
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+const PayForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  justify-content: center;
+`;
+
+const PayFormLabel = styled.label`
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+`;
+
+const PayFormInput = styled.input`
+  padding: 0.5rem;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  width: 20rem;
+  max-width: 100%;
+`;
+
+const StyledMaskedInput = styled(MaskedInput)`
+  padding: 0.5rem;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  width: 20rem;
+  max-width: 100%;
+`;
+
+const PayFormBtns = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 1rem;
+`;
+
+const Button = styled.button`
+  border: 0;
+  line-height: 2;
+  padding: 0 1.5rem;
+  font-size: 1rem;
+  text-align: center;
+  background-color: #51eaff;
+  border-radius: 0.5rem;
+  &:hover {
+    background-color: #d7fffe;
+    cursor: pointer;
+  }
+`;
 
 const ProviderPay = () => {
   const [enteredNumber, setEnteredNumber] = useState("");
@@ -11,24 +79,23 @@ const ProviderPay = () => {
   const [error, setError] = useState("");
 
   const numberChangeHadler = (event) => {
-      setEnteredNumber(event.target.value);
-  }
+    setEnteredNumber(event.target.value);
+  };
   const amountChangeHadler = (event) => {
     setEnteredAmount(event.target.value);
-  } 
+  };
 
   const errorHandler = () => {
-      setError(null);
-  }
+    setError(null);
+  };
 
   const submitHandler = (event) => {
-      event.preventDefault();
-      if (enteredNumber.length == 17 && enteredAmount <= 1000) {
-
-      }
-      setEnteredNumber('');
-      setEnteredAmount('');
-  }
+    event.preventDefault();
+    if (enteredNumber.length == 17 && enteredAmount <= 1000) {
+    }
+    setEnteredNumber("");
+    setEnteredAmount("");
+  };
 
   const data = [
     {
@@ -63,54 +130,72 @@ const ProviderPay = () => {
   const provider = data.find((item) => item.id == key);
   return (
     <>
-      {error && (
+      {/* {error && (
           <ErrorModal
             title={"Errror"}
             message={"Be warned"}
             onConfirm={errorHandler}
            />
-      )}
-      <main className={styles.payWindow}>
+      )} */}
+      <PayWindow>
         <h2>{provider.title}</h2>
-        <img src={provider.url} alt={provider.title} />
+        <PayWindowImg src={provider.url} alt={provider.title} />
         <form onSubmit={submitHandler}>
-          <div className={styles.payForm}>
-            <div className={styles.payForm}>
-              <label>Phone number</label>
+          <PayForm>
+            <PayForm>
+              <PayFormLabel>Phone number</PayFormLabel>
               {/* <input
                 type="tel"
                 placeholder="+7(XXX)XXX-XX-XX"
                 value={enteredNumber}
                 onChange={numberChangeHadler}
               /> */}
-              <MaskedInput
-                mask={[/[1-9]/," ", '(', /\d/,/\d/,/\d/, ')', " ", /\d/,/\d/,/\d/,"-", /\d/,/\d/, "-", /\d/,/\d/]}
-                className = {styles.Marked}
+              <StyledMaskedInput
+                mask={[
+                  /[1-9]/,
+                  " ",
+                  "(",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  ")",
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  "-",
+                  /\d/,
+                  /\d/,
+                  "-",
+                  /\d/,
+                  /\d/,
+                ]}
                 type="tel"
                 placeholder="+7(XXX)XXX-XX-XX"
                 value={enteredNumber}
                 onChange={numberChangeHadler}
               />
-            </div>
-            <div className={styles.payForm}>
+            </PayForm>
+            <PayForm>
               <label>Amount</label>
               <input
-                min='1'
-                max='1000'
+                min="1"
+                max="1000"
                 placeholder="1-1000 RUB"
                 type="number"
                 value={enteredAmount}
                 onChange={amountChangeHadler}
               />
-            </div>
-            <div className={styles.payFormBtns}>
-              <Link href="/"><button type="button">Go back</button></Link>
-              <button type="submit">Pay</button>
-            </div>
-            
-          </div>
+            </PayForm>
+            <PayFormBtns>
+              <Link href="/">
+                <Button type="button">Go back</Button>
+              </Link>
+              <Button type="submit">Pay</Button>
+            </PayFormBtns>
+          </PayForm>
         </form>
-      </main>
+      </PayWindow>
     </>
   );
 };
